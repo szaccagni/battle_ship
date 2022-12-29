dd56eeee789a1edfcd31b24b9ecbd94e77086d30
szaccagni <sarahzaccagni@gmail.com>
2022-12-29 17:25:07 -0500

    create squares occupied logic

--- js/main.js
+++ js/main.js
@@ -115,35 +115,69 @@ function dragDrop(e) {
     e.preventDefault()
     let dropX = (Math.round((e.offsetX - curShip.shipGrabbedX) / 49) * 49)+2
     let dropY = (Math.round((e.offsetY - curShip.shipGrabbedY) / 49) * 49)+1
+    getSquareOccupied(dropX,dropY,null)
     validateDrop(dropX,dropY)
     dropShip(dropX,dropY)
 }
 
 function clickShip(e) {
     getCurShip(e)
-    validateDrop(curShip.translateCordinates[0],curShip.translateCordinates[1])
+    curShip.rotated *= -1
     if (curShip && curShip.dom.parentElement.id.search('Board') > 0) {
+        getSquareOccupied(curShip.translateCordinates[0],curShip.translateCordinates[1],null)
+        validateDrop(curShip.translateCordinates[0],curShip.translateCordinates[1])
         rotateShip()
     }       
 }
 
-function getSquareOccupied(){
-
+function getSquareOccupied(x,y,starting){
+    let startingSquare = starting
+    // get starting square
+    // logic only for physically dropped ships
+    if (!startingSquare) {
+        const idxY = Math.ceil(y / 49)
+        const idxX = Math.ceil(x / 49)
+        startingSquare = letters[idxY-1]+numbers[idxX]
+    }
+    let squares = [startingSquare]
+    const startLetter = startingSquare[0]
+    const startNum = startingSquare[1]
+    // horizontal
+    if (curShip.rotated === -1) {
+        for (let i = 1; i < curShip.width; i++ ) {
+            const numIdx = numbers.indexOf(startNum)
+            const num = numbers[numIdx + i]
+            const sqr = startLetter + num
+            squares.push(sqr)     
+        }
+    } else { // vertical 
+        for (let i = 1; i < curShip.width; i++ ) {
+            const letterIdx = letters.indexOf(startLetter)
+            const letter = letters[letterIdx + i]
+            const sqr = letter + startNum
+            squares.push(sqr)   
+        }
+    }
+    console.log(squares)
 }
 
 function validateDrop(x,y) {
-    // check horizontal placemenet
-    if ((x + curShip.dom.offsetWidth > 494) || (x < 0)) {
-        console.log('x is off', x, curShip.dom.offsetWidth)
-    } else {
-        console.log('x is ok')
-    }
-    // check vertical placement
-    if ((y + curShip.dom.firstChild.offsetWidth > 494) || (y < 0)){
-        console.log('y is off', y, curShip.dom.firstChild.offsetWidth)
-    } else {
-        console.log('y is okay')
-    }
+    // // check horizontal placemenet
+    // if ((x + curShip.dom.offsetWidth > 494) || (x < 0)) {
+    //     console.log('x is off', x, curShip.dom.offsetWidth)
+    // } else {
+    //     console.log('x is ok')
+    // }
+    // // check vertical placement
+    // if ((y + curShip.dom.firstChild.offsetWidth > 494) || (y < 0)){
+    //     console.log('y is off', y, curShip.dom.firstChild.offsetWidth)
+    // } else {
+    //     console.log('y is okay')
+    // }
+    // // check squares are not already occupied by a ship
+    // // check current players board that all squares to be occupied are null 
+    // // else return a falsy
+    // // basically if another ship is there you cannot place
 }
 
 /*----- render functions -----*/
@@ -206,29 +240,28 @@ function dropShip(x,y) {
 }
 
 function rotateShip() {
-    curShip.dom.style.width = curShip.rotated === 1 ? `${curShip.width * 3}rem` : '3rem'
+    curShip.dom.style.width = curShip.rotated === -1 ? `${curShip.width * 3}rem` : '3rem'
     let rotateBy
     if (curShip.width % 2 === 0) {
-        if (curShip.rotated === -1) {
+        if (curShip.rotated === 1) {
             rotateBy = (curShip.width * 12) + (25 * (curShip.width/2 - 1)) - (curShip.width/2 - 1)
         } 
     } else {
-        if (curShip.rotated === -1) {
+        if (curShip.rotated === 1) {
             rotateBy = (Math.round(((curShip.width * 12) + 25)/48) * 48) + 1  
         } 
     }
-    curShip.dom.firstChild.style.transform = curShip.rotated === 1 ? '' : `translate(-${rotateBy}px,${rotateBy}px) rotate(90deg)`
-    curShip.rotated *= -1
+    curShip.dom.firstChild.style.transform = curShip.rotated === -1 ? '' : `translate(-${rotateBy}px,${rotateBy}px) rotate(90deg)`
 }
 
 
 init()
 
 // // // testing
-// // player1BoardEl.addEventListener('click', test)
-// // function test(e){
-// //     testEl = curPlayer.ships.find(ship => ship.name === curShip.dom.id)
-// //     testEl.test = 'hi'
-// //     console.log(testEl)
-
-// // }
+// player1BoardEl.addEventListener('click', test)
+// function test(e){
+//     let idxX = Math.ceil(e.offsetX / 49)
+//     console.log(e.offsetX, idxX, numbers[idxX])
+//     let idxY = Math.ceil(e.offsetY / 49)
+//     console.log(e.offsetY, idxY, letters[idxY-1])
+// }
